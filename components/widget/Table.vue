9833b03c89a4b06a79dc8435b88622337724cfc3
Fan Kangtai <fankangtai@gmail.com>
2023-04-07 14:25:57 +0800

    Create a simple table component

---
+++ components/widget/Table.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="overflow-x-auto">
+        <table class="table w-full" data="">
+            <!-- head -->
+            <thead>
+            <tr>
+                <th></th>
+                <th v-for="h in headers" :key="h.title">{{ h.title }}</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(d, rowIndex) in data" :key="d.id">
+                <th>{{ rowIndex + 1 }}</th>
+                <td v-for="(h, columnIndex) in headers" :key="`${rowIndex}${columnIndex}`">{{ d[h.value] }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    headers: {
+        type: Array,
+        required: true,
+        default: () => []
+    },
+    data: {
+        type: Array,
+        required: true,
+        default: () => []
+    }
+})
+</script>
--- pages/keys.vue
+++ pages/keys.vue
@@ -1,10 +1,41 @@
 <template>
     <div>
         <h1>Keys</h1>
+        <div class="mt-2">
+            <widget-table :headers="headers" :data="tableData"/>
+        </div>
     </div>
 </template>
 
 <script setup>
+const headers = [
+    {title: 'Name', value: 'name'},
+    {title: 'Job', value: 'job'},
+    {
+        title: 'FavoriteColor',
+        value: 'favoriteColor'
+    }
+    ]
+const tableData = [
+    {
+        id: 1,
+        name: 'Cy Ganderton',
+        job: 'Quality Control Specialist',
+        favoriteColor: 'Blue'
+    },
+    {
+        id: 2,
+        name: 'Hart Hagerty',
+        job: 'Desktop Support Technician',
+        favoriteColor: 'Purple'
+    },
+    {
+        id: 3,
+        name: 'Brice Swyre',
+        job: 'Tax Accountant',
+        favoriteColor: 'Red'
+    }
+]
 </script>
 
 <style scoped>
